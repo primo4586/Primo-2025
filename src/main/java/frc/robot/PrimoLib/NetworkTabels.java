5dbe77addbdc3078187ffc52f76ee54da81c23a3
eilonhuppert <eilonhuppert@gmail.com>
2024-11-21 13:44:22 +0200

    Created project

---
+++ src/main/java/frc/robot/PrimoLib/NetworkTabels.java
@@ -0,0 +1,64 @@
+package frc.robot.PrimoLib;
+
+import edu.wpi.first.networktables.DoubleArrayPublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StringPublisher;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Misc;
+import frc.robot.subsystems.Vision.AprilTagCamera;
+import frc.robot.subsystems.Vision.ObjectDetectionCamera;
+import frc.robot.subsystems.Vision.VisionConstants;
+public class NetworkTabels {
+    //subsystems
+
+    // cameras 
+    private static final AprilTagCamera leftAprilTagCamera = Misc.LEFT_CAMERA;
+    private static final AprilTagCamera rightAprilTagCamera = Misc.RIGHT_CAMERA;
+    private static final ObjectDetectionCamera noteCamera = Misc.NOTE_CAMERA;
+
+    // swerve 
+
+
+
+    // clock 
+    private static double _clock;
+    private static double _countDown;
+
+    // field 
+    private static final Field2d m_field = new Field2d();
+
+
+    public static void updateValues(){
+        updateClock();
+        updateVision();
+        // updateRobotPose();
+    }
+
+    //clock func
+    public static void setClock(double clock, double countDown){
+        _clock = clock;
+        _countDown = countDown;
+    }
+    public static void updateClock(){
+        _clock -= _countDown;
+        SmartDashboard.putNumber("clock", _clock);
+        if (_clock <= 0){setClock(0, 0);}
+    }
+
+
+    // field func
+    public static void setField(){SmartDashboard.putData("Field", m_field);}
+
+    public static void updateRobotPose(){
+        m_field.setRobotPose(Misc.m_Telemetry.getRobotPose());}
+
+    //vision
+    public static void updateVision(){
+        SmartDashboard.putBoolean("April tag detection", leftAprilTagCamera.seeTarget() || rightAprilTagCamera.seeTarget());
+    }
+
+
+
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,86 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.subsystems.swerve.CommandSwerveDrivetrain;
+import frc.robot.subsystems.swerve.Telemetry;
+import frc.robot.subsystems.swerve.TunerConstants;
+
+public class RobotContainer {
+
+
+
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  private final CommandXboxController driverController = new CommandXboxController(0); // My joystick
+  private final CommandXboxController operaController = new CommandXboxController(1);
+  private final CommandSwerveDrivetrain drivetrain = TunerConstants.Swerve; // My drivetrain
+
+    private DoubleSupplier speed = () -> TunerConstants.kSpeedAt12VoltsMps  - driverController.getHID().getLeftTriggerAxis() * 0.8;
+  private double MaxAngularRate = 3 * Math.PI;//1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
+
+
+  private final SwerveRequest.FieldCentric driveFieldCentric = new SwerveRequest.FieldCentric()
+      .withDeadband(TunerConstants.kSpeedAt12VoltsMps * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+
+  private final SwerveRequest.RobotCentric driveRobotCentric = new SwerveRequest.RobotCentric()
+  .withDeadband(MaxAngularRate * 0.1).withRotationalDeadband(MaxAngularRate * 0.1).
+  withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+
+  private final SwerveRequest.FieldCentricFacingAngle driveFacingAngle = new SwerveRequest.FieldCentricFacingAngle()
+  .withDeadband(MaxAngularRate * 0.1).withRotationalDeadband(MaxAngularRate * 0.1).
+  withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+
+
+                                                               // driving in open loop
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+
+  private final Telemetry logger = Misc.m_Telemetry;
+
+  private void configureBindings() {
+    // swerve command
+    drivetrain.setDefaultCommand(drivetrain.applyRequest(() -> driveFieldCentric.withVelocityX(driverController.getRightX() * speed.getAsDouble()) //fieldCentric
+        .withVelocityY(driverController.getLeftY() * speed.getAsDouble())
+        .withRotationalRate(driverController.getRightY() * MaxAngularRate)));
+      
+
+    driverController.leftBumper().whileTrue(drivetrain.applyRequest(() ->
+      brake));
+
+
+      // reset the field-centric heading on left bumper press
+      driverController.y().onTrue(drivetrain.runOnce(() ->
+      drivetrain.seedFieldRelative()));
+    drivetrain.registerTelemetry(logger::telemeterize);
+    
+
+
+
+  }
+
+  public RobotContainer() {
+    configureBindings();
+
+
+		// driverController.back().and(driverController.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+		// driverController.back().and(driverController.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+		// driverController.start().and(driverController.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+		// driverController.start().and(driverController.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+  }
+}
